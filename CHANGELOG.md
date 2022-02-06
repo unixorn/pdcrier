10c861950a4e85f41b5124ac90ba090d294a5ba6
Joe Block <jpb@unixorn.net>
2022-02-06 08:51:08 -0700

    Use incident_key for alert deduplication
    - Pass PagerDuty an incident_key so we don't generate duplicate incidents for
      the same issue.  If we don't get an incident key, use the message title.
    - Display incident key in created incidents when present
    - Catch exception when there's a duplicated incident_key
    - Print PD errors when we fail to create an incident.
    
    It's usually because there's already an incident with that incident_key
    and PagerDuty is refusing to duplicate it, but whatever it is, log
    whatever PagerDuty is complaining about.
    
    Signed-off-by: Joe Block <jpb@unixorn.net>

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# pdcrier
+
+## 0.1.3
+
+First release.
+
+## 0.2.0
+
+Adds use of `incident_key` to prevent unwanted alert replication.
--- pdcrier/cli/alerts.py
+++ pdcrier/cli/alerts.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #
-# Create a pagerduty alert
+# Create a pagerduty alert from the command line
 #
 # Copyright 2022, Joe Block <jpb@unixorn.net>
 # License: Apache 2.0
 
 import argparse
 import logging
-import os
+import sys
 
 from pdcrier.pagerduty import PagerDuty, loadCrierSettings
 
@@ -26,24 +26,19 @@ def parseCLI():
         choices=["DEBUG", "INFO", "ERROR", "WARNING", "CRITICAL"],
         default="INFO",
     )
-    if "HOME" in os.environ:
-        parser.add_argument(
-            "--settings-file",
-            "--settings",
-            type=str,
-            help="Path to a settings file. Settings in the file are overridden by command line options",
-            default=f"{os.environ.get('HOME')}/.hass-tools/pagerduty.yaml",
-        )
-    else:
-        parser.add_argument(
-            "--settings-file",
-            "--settings",
-            type=str,
-            default="/config/pagerduty.yaml",
-            help="Path to a settings file. Settings in the file are overridden by command line options",
-        )
-
+    parser.add_argument(
+        "--settings-file",
+        "--settings",
+        type=str,
+        default="/config/pagerduty.yaml",
+        help="Path to a settings file. Settings in the file are overridden by command line options",
+    )
     # Alert options
+    parser.add_argument(
+        "--incident-key",
+        help="Incident key - used to ensure no duplicate incidents. Will default to title if unset",
+        type=str,
+    )
     parser.add_argument(
         "--message",
         help="Alert message body - this won't be visible in SMSes",
@@ -53,7 +48,7 @@ def parseCLI():
     parser.add_argument("--service-id", help="Service ID to create an alert")
     parser.add_argument(
         "--title",
-        help="Title of alert - this is all you'll see via SMS",
+        help="Title of alert - this is all you'll see via SMS, so be specific",
         type=str,
         required=True,
     )
@@ -73,17 +68,47 @@ def alerter():
     """
     cli = parseCLI()
     settings = loadCrierSettings(cli=cli)
+    if "incident_key" not in settings:
+        logging.warning(f"incident_key not specified, using title '{cli.title}'")
+        settings["incident_key"] = cli.title
+
     pd = PagerDuty(
         api_token=settings["pagerduty-api-token"], sender=settings["default-sender"]
     )
     incident = pd.createIncident(
-        title=cli.title, service_id=settings["service_id"], message=cli.message
+        title=cli.title,
+        service_id=settings["service_id"],
+        incident_key=settings["incident_key"],
+        message=cli.message,
     )
-    logging.debug(f"Created incident {incident}.")
-    logging.info(f"Incident number: {incident['incident_number']}")
-    logging.info(f"Incident title: {incident['title']}")
-    logging.info(f"Description: {incident['description']}")
-    logging.info(f"Created At: {incident['created_at']}")
+    try:
+        logging.debug(f"Created incident {incident}.")
+        logging.info(f"Incident title: {incident['title']}")
+        logging.info(f"Incident number: {incident['incident_number']}")
+        if "incident_key" in incident:
+            logging.info(f"Incident key: {incident['incident_key']}")
+        else:
+            logging.debug("No incident key, allowing duplicates")
+        logging.info(f"Description: {incident['description']}")
+        logging.info(f"Created At: {incident['created_at']}")
+    except KeyError:
+        if "exception-response" in incident:
+            error_status = 13
+            error = incident["exception-response"].response.json()["error"]
+            for error_message in error["errors"]:
+                logging.warning(f"{error_message}")
+                if (
+                    error_message
+                    == "Open incident with matching dedup key already exists on this service"
+                ):
+                    # We don't want shell scripts calling us to fail when an
+                    # incident already exists, so we explicitly exit 0 when
+                    # we find a duplicate.
+                    error_status = 0
+                    logging.warning("Do not need a new incident, one already exists.")
+            if error_status != 0:
+                logging.error("Could not create new incident!")
+            sys.exit(error_status)
 
 
 if __name__ == "__main__":
